97cf7c32b77fd6c92fff0eac2863dfe2afb99e76
Emmanuel Levijarvi <emansl@gmail.com>
2025-09-27 11:45:42 -0700

    fingers crossed

--- navien_nwp500/_version.py
+++ navien_nwp500/_version.py
@@ -27,7 +27,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.0.post14+dirty"
-__version_tuple__ = version_tuple = (0, 0, "post14", "dirty")
+__version__ = version = "0.0.post20+dirty"
+__version_tuple__ = version_tuple = (0, 0, "post20", "dirty")
 
-__commit_id__ = commit_id = "g558b81f1e"
+__commit_id__ = commit_id = "g6a154eb7a"
