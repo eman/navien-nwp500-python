bb05d4ae6b60868cd108c58553e35be1b6026f4d
Emmanuel Levijarvi <emansl@gmail.com>
2025-09-27 17:48:41 -0700

    version

--- navien_nwp500/_version.py
+++ navien_nwp500/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "1.0.0.post7+dirty"
-__version_tuple__ = version_tuple = (1, 0, 0, "post7", "dirty")
+__version__ = version = '1.0.0.post11+dirty'
+__version_tuple__ = version_tuple = (1, 0, 0, 'post11', 'dirty')
 
-__commit_id__ = commit_id = "gf15c2b1eb"
+__commit_id__ = commit_id = 'gfd1319b05'
